474a051d39e9ae1ca0470ff50d9a514ab8eae837
LucasRojas <rolukasro12@gmail.com>
2024-06-22 01:45:11 -0300

    trabajo terminado

--- app/Views/front/header.php
+++ app/Views/front/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>GymFit</title>
 
     <!-- Agrege un css para modificar a mi gusto-->
     <link href="assets/css-mi-Estilo/style.css" rel="stylesheet">
